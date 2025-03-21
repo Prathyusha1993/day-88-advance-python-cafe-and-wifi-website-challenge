70239650ccb84171e1a5cefe816c9f657604da11
prathyusha1993 <prathyusha9310@gmail.com>
2025-03-21 14:10:33 -0700

    Removed .env file from Git tracking and added add and delte functionality for cafe website

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import smtplib
-
-from flask import Flask, render_template, url_for, redirect, jsonify, request
+from flask import Flask, render_template, url_for, redirect, jsonify, request, flash
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
 import os
 from dotenv import load_dotenv
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.fields.simple import BooleanField
+from wtforms.validators import DataRequired, URL, Optional, InputRequired
 
 load_dotenv()
 
@@ -45,6 +48,20 @@ class Cafe(db.Model):
 with app.app_context():
     db.create_all()
 
+
+class CafeForm(FlaskForm):
+    name = StringField('Cafe name', validators=[DataRequired()])
+    map_url = StringField('Maps URL', validators=[DataRequired(), URL()])
+    img_url = StringField('Image URL', validators=[DataRequired(), URL()])
+    location = StringField('Location', validators=[DataRequired()])
+    has_sockets = BooleanField('Sockets',  validators=[InputRequired()], default=False)
+    has_toilet = BooleanField('Toilets', validators=[InputRequired()], default=False)
+    has_wifi = BooleanField('Wifi', validators=[InputRequired()], default=False)
+    can_take_calls = BooleanField('Can Take Calls', validators=[InputRequired()], default=False)
+    seats = StringField('Seats', validators=[DataRequired()])
+    coffee_price = StringField('Coffee Price', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -53,7 +70,7 @@ def home():
 def cafes():
     result = db.session.execute(db.select(Cafe).order_by(Cafe.name)).scalars().all()
     list_of_cafes =  [cafe.to_dict() for cafe in result]
-    print(f'list of cafes: {list_of_cafes}')
+    # print(f'list of cafes: {list_of_cafes}')
     return render_template('cafe.html', cafes=list_of_cafes)
 
 @app.route('/about')
@@ -75,6 +92,42 @@ def send_email(name, email, phone, message):
         connection.starttls()
         connection.login(os.getenv('MY_EMAIL'), os.getenv('MY_PASSWORD'))
         connection.sendmail(os.getenv('MY_EMAIL'), os.getenv('MY_EMAIL'), email_message)
+
+# add cafe
+@app.route('/add',methods=['GET','POST'])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        new_cafe_details = Cafe(
+            name = form.name.data,
+            map_url=form.map_url.data,
+            img_url=form.img_url.data,
+            location=form.location.data,
+            has_sockets=form.has_sockets.data,  # ✅ Directly use WTForms data
+            has_toilet=form.has_toilet.data,
+            has_wifi=form.has_wifi.data,
+            can_take_calls=form.can_take_calls.data,
+            seats=form.seats.data,
+            coffee_price=form.coffee_price.data
+        )
+        db.session.add(new_cafe_details)
+        db.session.commit()
+        return redirect(url_for('cafes'))
+    return render_template('add.html', form=form)
+
+# delete cafe
+@app.route('/delete/<int:cafe_id>',methods=['GET','POST'])
+def delete_cafe(cafe_id):
+    cafe_to_delete = db.session.get(Cafe, cafe_id)
+    if cafe_to_delete:
+        db.session.delete(cafe_to_delete)
+        db.session.commit()
+        flash('Cafe deleted successfully!', 'success')
+    else:
+        flash('cafe not found', 'danger')
+    return redirect(url_for('cafes'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
