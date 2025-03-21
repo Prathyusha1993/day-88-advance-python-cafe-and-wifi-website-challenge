98c3527ee56ed4ba681fadb0f181251fd0ace7b1
prathyusha1993 <prathyusha9310@gmail.com>
2025-03-20 17:16:20 -0700

    day 88 advance python web development cafes and wifi project

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from http.cookiejar import debug
+import smtplib
 
-from flask import Flask, render_template, url_for, redirect, jsonify
+from flask import Flask, render_template, url_for, redirect, jsonify, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -48,9 +52,29 @@ def home():
 @app.route('/cafes')
 def cafes():
     result = db.session.execute(db.select(Cafe).order_by(Cafe.name)).scalars().all()
-    return jsonify(cafes=[cafe.to_dict() for cafe in result])
+    list_of_cafes =  [cafe.to_dict() for cafe in result]
+    print(f'list of cafes: {list_of_cafes}')
+    return render_template('cafe.html', cafes=list_of_cafes)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        data = request.form
+        send_email(data['name'], data['email'],data['phone'], data['message'])
+        return render_template('contact.html', msg_sent=True)
+    return render_template('contact.html', msg_sent=False)
 
 
+def send_email(name, email, phone, message):
+    email_message = f'Subject: New Message\n\n Name: {name}\nEMail:{email}\nPhone:{phone}\nMessage:{message}'
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(os.getenv('MY_EMAIL'), os.getenv('MY_PASSWORD'))
+        connection.sendmail(os.getenv('MY_EMAIL'), os.getenv('MY_EMAIL'), email_message)
 
 if __name__ == '__main__':
     app.run(debug=True)
